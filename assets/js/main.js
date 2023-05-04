915e7f4813602f5e458da72aebf1aa61809d629e
Alexandre Paiva <alexandremarcio1504@gmail.com>
2023-05-04 20:40:11 -0300

    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,36 @@ const limit = 20;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit};`;
 
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        <li id="type-grass" class="type" >Grass</li>
+                        <li id="type-poison" class="type">Poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif" alt="${pokemon.name}">
+                </div>
+    </li>
+            `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) =>console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+
+           
+        }
+    })
     .catch((error) => console.error(error))
+
     
