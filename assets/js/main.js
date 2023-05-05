1c9d011142ae79f9130db3996d102b84b88b152a
Alexandre Paiva <109223675+OpsXandao@users.noreply.github.com>
2023-05-05 18:45:09 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,27 @@
-
-const offset = 0;
-const limit = 20;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit};`;
+function convertPokemonTypesToLi(pokemonsTypes){
+    return pokemonsTypes.map((typeSlot) =>`<li class="type" >${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon){
     return `
-    <li class="pokemon">
-                <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span>
-                
-                <div class="detail">
-                    <ol class="types">
-                        <li id="type-grass" class="type" >Grass</li>
-                        <li id="type-poison" class="type">Poison</li>
-                    </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif" alt="${pokemon.name}">
-                </div>
-    </li>
-            `
+        <li class="pokemon">
+                    <span class="number">#${pokemon.order}</span>
+                    <span class="name">${pokemon.name}</span>
+                    
+                    <div class="detail">
+                        <ol class="types">
+                           ${convertPokemonTypesToLi(pokemon.types).join('')}
+                        </ol>
+                        <img src="${pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']}" alt="${pokemon.name}">
+                    </div>
+        </li>
+     `
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-           
-        }
-    })
-    .catch((error) => console.error(error))
-
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML += newHtml
+})
     
